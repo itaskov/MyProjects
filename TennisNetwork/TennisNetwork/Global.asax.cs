98aeef27d6691961292bd30a18c68f7d372d5c16
itaskov <itaskov@gmail.com>
2015-01-30 18:23:14 +0200

    Revert "Restructure files."
    This reverts commit 204ac0c560b8223bee4079b4484aa3107587caef.

---
+++ TennisNetwork/TennisNetwork/Global.asax.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using TennisNetwork.Data;
+
+namespace TennisNetwork
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            Database.SetInitializer<ApplicationDbContext>(new MigrateDatabaseToLatestVersion<ApplicationDbContext, TennisNetwork.Data.Migrations.Configuration>());
+            Automapper.AutomapperConfig.Map();
+        }
+    }
+}
