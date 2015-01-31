974759004d6693ecdf21e4eaf7da97d5593e99ad
itaskov <itaskov@gmail.com>
2015-01-31 21:45:10 +0200

    Delete all files.

--- TennisNetwork/TennisNetwork/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TennisNetwork.MvcApplication" Language="C#" %>
--- TennisNetwork/TennisNetwork/Scripts/datepickerReady.js
+++
@@ -1,8 +0,0 @@
-﻿//if (!Modernizr.inputtypes.date) {
-
-//    $(function () {
-
-//        $(".datepicker").datepicker();
-
-//    });
-//}
